b40727c9df637ae8fba7e56867df16df9eee7164
NunoSempere <nuno.sempere@gmail.com>
2021-11-25 09:01:31 +0000

    tweaks: Id now alphabetical strings rather than numbers

--- pages/index.js
+++ pages/index.js
@@ -221,9 +221,9 @@ export default function Home({ listOfElementsDefault }) {
   }
 
   let nextStepSlider = ({ posList, binaryComparisons, sliderValue, element1, element2 }) => {
-    if (sliderValue < 1) {
-      // sliderValue = -sliderValue;
-      // [element1, element2] = [element2, element1]
+    if (sliderValue < 1 && sliderValue > 0) {
+      sliderValue = 1/sliderValue;
+      [element1, element2] = [element2, element1]
     }
     console.log(`posList@nextStepSlider:`)
     console.log(posList)
