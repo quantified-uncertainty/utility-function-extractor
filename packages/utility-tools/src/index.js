b94c66a78a56734688f61ba34fc954b2557cf9a8
NunoSempere <nuno.sempere@protonmail.com>
2022-06-17 14:34:54 -0400

    fix: exports

--- packages/utility-tools/src/index.js
+++ packages/utility-tools/src/index.js
@@ -2,6 +2,5 @@ import { mergeSort } from "./mergeSort.js";
 import { findDistancesFromAllElementsToAllReferencePoints } from "./findPaths.js";
 import { aggregatePaths } from "./aggregatePaths.js";
 
-export const mergeSort = mergeSort;
 export const findPaths = findDistancesFromAllElementsToAllReferencePoints;
-export const aggregatePaths = aggregatePaths;
+export { mergeSort, aggregatePaths };
