f11baba4231873a0455ed4a3e483994a249766cf
NunoSempere <nuno.sempere@protonmail.com>
2022-06-19 20:27:13 -0400

    fix: class -> className

--- packages/webpage-refactor/components/separator.js
+++ packages/webpage-refactor/components/separator.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export function Separator() {
   return (
-    <div class="py-4">
-      <div class="w-full border-t border-4 border-gray-300 mt-10"></div>
+    <div className="py-4">
+      <div className="w-full border-t border-4 border-gray-300 mt-10"></div>
     </div>
   );
 }
